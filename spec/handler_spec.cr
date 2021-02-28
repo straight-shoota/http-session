249559dcfa4dd73ae2e6215bd9e750d3fee09dc7
Johannes Müller <straightshoota@gmail.com>
2021-02-28 01:02:50 +0100

    Add spec for invalid session id

--- spec/handler_spec.cr
+++ spec/handler_spec.cr
@@ -21,6 +21,24 @@ describe HTTP::Session::Handler do
     response.body.should eq "session_id: UTA54jUvEQE1nVDTSi-TCw"
   end
 
+  it "overrides invalid session cookie" do
+    storage = HTTP::Session::Storage::Memory.new
+    handler = HTTP::Session::Handler.new(storage)
+    handler.next = ->(context : HTTP::Server::Context) do
+      context.response.print "session_id: #{context.session.session_id}"
+    end
+    handler.random = Random.new(1)
+
+    request = HTTP::Request.new("GET", "/")
+    request.cookies << handler.base_cookie.dup.tap do |request_cookie|
+      request_cookie.value = "invalid"
+    end
+    response = handle_http_request(handler, request)
+    cookie = response.cookies["session_id"]
+    cookie.value.should eq "UTA54jUvEQE1nVDTSi-TCw"
+    response.body.should eq "session_id: UTA54jUvEQE1nVDTSi-TCw"
+  end
+
   it "#terminate_session" do
     storage = HTTP::Session::Storage::Memory.new
     handler = HTTP::Session::Handler.new(storage)
